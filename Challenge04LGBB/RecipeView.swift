791be06672a8586164caf0743db3b514312f0d9f
Luciano Uchoa <lucianou54@gmail.com>
2022-08-16 12:07:58 -0400

    Adicionando a montagem da tela

---
+++ Challenge04LGBB/RecipeView.swift
@@ -0,0 +1,33 @@
+//
+//  RecipeView.swift
+//  Challenge04LGBB
+//
+//  Created by Luciano Uchoa on 16/08/22.
+//
+
+import Foundation
+import UIKit
+
+class Recipe {
+    var tituloReceita: String
+    var numeroIntrucoes: Int
+    var descricaoReceita : [String]
+    var pessoaTurno: [String]
+    var numeroEtapas: Int
+    
+    init(tituloReceita: String, numeroIntrucoes: Int, pessoaTurno: [String], descricaoReceita: [String], numeroEtapas: Int){
+        self.tituloReceita = tituloReceita
+        self.numeroIntrucoes = numeroIntrucoes
+        self.pessoaTurno = pessoaTurno
+        self.descricaoReceita = descricaoReceita
+        self.numeroEtapas = numeroEtapas
+    }
+    
+    init(){
+        self.tituloReceita = ""
+        self.numeroIntrucoes = 0
+        self.pessoaTurno = [""]
+        self.descricaoReceita = [""]
+        self.numeroEtapas = 0
+    }
+}
