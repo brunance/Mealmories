f608817d1b4c80eeab53c4af140c312394fd0f36
gabrieldigon <gabrieldiasgonca@gmail.com>
2022-08-16 12:09:14 -0400

    Agora conseguimos voltar para as telas anteriores

--- Challenge04LGBB/FirstPage.swift
+++ Challenge04LGBB/FirstPage.swift
@@ -36,9 +36,24 @@ class FirstPage: UIViewController {
         newViewController.modalPresentationStyle = .fullScreen
         self.present(newViewController, animated: false, completion: nil)
         
-       
         
+    }
+    
+    
+    @IBAction func SwipeRight(_ sender: Any) {
+        cont -= 1
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let newViewController = storyBoard.instantiateViewController(withIdentifier: "Main") as! FirstPage
         
+
+        let transition = CATransition()
+            transition.duration = 0.4
+            transition.type = CATransitionType.reveal
+            transition.subtype = CATransitionSubtype.fromLeft
+            guard let window = view.window else { return }
+            window.layer.add(transition, forKey: kCATransition)
+        newViewController.modalPresentationStyle = .fullScreen
+        self.present(newViewController, animated: false, completion: nil)
     }
     
 
