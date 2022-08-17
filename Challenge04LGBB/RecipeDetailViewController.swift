9bd5bf440a0b734790b54dc9c581b6468db178e7
Luciano Uchoa <lucianou54@gmail.com>
2022-08-17 09:05:10 -0400

    Adicionar informacoes da classe na tela

--- Challenge04LGBB/RecipeDetailViewController.swift
+++ Challenge04LGBB/RecipeDetailViewController.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+
+
+class RecipeDetailViewController: UIViewController{
+    
+    var recipes : [Recipe] = []
+        
+    @IBOutlet weak var labelIntrucao: UILabel!
+    @IBOutlet weak var imagemIntrucao: UIImageView!
+    @IBOutlet weak var labelTurno: UILabel!
+    @IBOutlet weak var labelImagem: UILabel!
+    @IBOutlet weak var labelContagemTurno: UILabel!
+
+    
+    override func viewDidLoad() {
+        
+        recipes = [
+            Recipe(tituloReceita: "Pão de Queijo", numeroIntrucoes: 9, pessoaTurno: ["Adulto, Criança, Mix, Adulto, Mix, Criança, Mix, Adulto, Adulto"], descricaoReceita: ["Em uma vasilha, junte os ingredientes secos: polvilho doce, o parmesão", "misture bem com uma colher ou com a mão!", "Adicione o creme de leite, aos poucos, misturando com as mãos até formar uma massa homogênea e firme.","Retire porções pequenas da massa", "modele do formato que quiser, bolinhas, dadinhos, seja criativo!", "Unte uma fôrma com manteiga e trigo, papel manteiga ou spray de untar ", "Coloque uma ao lado da outra na fôrma grande untada", "Leve ao forno alto, preaquecido, por 15 minutos ou até dourar.", "Retire e sirva em seguida."], numeroEtapas: 3, imagemIntrucao: UIImage(named: "imagePlaceholder")!)
+        ]
+        
+        imagemIntrucao.image = recipes[0].imagemIntrucao
+        labelIntrucao.text = recipes[0].descricaoReceita[0]
+        labelTurno.text = "Turno \(recipes[0].numeroEtapas)"
+    }
+}
