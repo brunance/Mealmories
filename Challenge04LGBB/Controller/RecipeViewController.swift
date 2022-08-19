cc41f522d210d7b849db67918c9b5ca1d9c1d099
gabrieldigon <gabrieldiasgonca@gmail.com>
2022-08-19 09:03:22 -0400

    FIx: fix Ui elements

--- Challenge04LGBB/Controller/RecipeViewController.swift
+++ Challenge04LGBB/Controller/RecipeViewController.swift
@@ -17,14 +17,16 @@ class RecipeViewController: UIViewController{
     @IBOutlet weak var ImagemInstrucao: UIImageView!
     @IBOutlet weak var LInhaDivisoria: UIView!
     @IBOutlet weak var statusbar: UIView!
+    @IBOutlet weak var ContDeInstrucoes: UILabel!
     
+    @IBOutlet weak var LampImage: UIImageView!
     @IBOutlet weak var LabelDica: UILabel!
     override func viewDidLoad() {
        
         viewTurno.layer.cornerRadius = 15
         viewTurno.layer.borderWidth = 5
         recipes = [
-            Recipe(tituloReceita: "Pão de Queijo", numeroIntrucoes: 9, pessoaTurno: ["Adulto", "Criança", "Mix", "Adulto", "Mix", "Criança", "Mix", "Adulto", "Adulto"], descricaoReceita: ["Em uma vasilha, junte os ingredientes secos: polvilho doce, e parmesão", "misture bem com uma colher ou com a mão!", "Adicione o creme de leite, aos poucos, misturando com as mãos até formar uma massa homogênea e firme.","Retire porções pequenas da massa", "modele do formato que quiser, bolinhas, dadinhos, seja criativo!", "Unte uma fôrma com manteiga e trigo, papel manteiga ou spray de untar ", "Coloque uma ao lado da outra na fôrma grande untada", "Leve ao forno alto, preaquecido, por 15 minutos ou até dourar.", "Retire e sirva em seguida."], numeroEtapas: 3,
+            Recipe(tituloReceita: "Pão de Queijo", numeroIntrucoes: 9, pessoaTurno: ["Adulto", "Criança", "Mix", "Adulto", "Mix", "Criança", "Mix", "Adulto", "Adulto"], descricaoReceita: ["Em uma vasilha, junte os ingredientes secos: polvilho doce, e parmesão", "misture bem com uma colher ou com a mão!", "Adicione o creme de leite, aos poucos, misturando com as mãos até formar uma massa homogênea e firme.","Retire porções pequenas da massa", "modele do formato que quiser, bolinhas, dadinhos, seja criativo!", "Unte uma fôrma com manteiga e trigo, papel manteiga ou spray de untar ", "Coloque uma ao lado da outra na fôrma grande untada", "Leve ao forno alto, preaquecido a 180°C , por 15 minutos ou até dourar.", "Retire e sirva em seguida."], numeroEtapas: 3,
                    imagemIntrucao:
                     [UIImage(named: "secos.1")!
                      ,UIImage(named: "mistura.1")!
@@ -35,7 +37,7 @@ class RecipeViewController: UIViewController{
                      ,UIImage(named: "Organizarnaforma.1")!
                      ,UIImage(named: "forno.1")!,
                      UIImage(named: "paodequeijo.1")!]
-                   ,CorDaTela: [UIColor(named: "Adulto_Blue")!,UIColor(named: "Child_Orange")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Adulto_Blue")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Child_Orange")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Adulto_Blue")!,UIColor(named: "Adulto_Blue")!],dicas: ["Qualquer dos dois tipos de polvilhos são bem-vindos, variando de acordo com o gosto de quem manuseia a receita","","É necessário que o creme de leite seja adicionado aos poucos, para não perder o ponto da massa.","","É necessário que o creme de leite seja adicionado aos poucos, para não perder o ponto da massa.","Pode haver ajuda do adulto na orientação, mas é interessante que a criança faça sozinha.","Deixe sempre um pequeno espaço entre, pode facilitar na hora de retirar os pães.","Deixe por 15 minutos ou até dourar",""])
+                   ,CorDaTela: [UIColor(named: "Adulto_Blue")!,UIColor(named: "Child_Orange")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Adulto_Blue")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Child_Orange")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Adulto_Blue")!,UIColor(named: "Adulto_Blue")!],dicas: ["Qualquer dos dois tipos de polvilhos são bem-vindos, variando de acordo com o gosto de quem manuseia a receita","","É necessário que o creme de leite seja adicionado aos poucos, para não perder o ponto da massa.","","É necessário que o creme de leite seja adicionado aos poucos, para não perder o ponto da massa.","Pode haver ajuda do adulto na orientação, mas é interessante que a criança faça sozinha.","Deixe sempre um pequeno espaço entre as massas , pode facilitar na hora de retirar os pães.","",""])
         ]
        
         labelTurno.text = "Turno \(recipes[0].pessoaTurno[count])"
@@ -46,6 +48,10 @@ class RecipeViewController: UIViewController{
         viewTurno.layer.borderColor = recipes[0].CorDaTela[count].cgColor
         LInhaDivisoria.backgroundColor = recipes[0].CorDaTela[count]
         imagemIntrucao.image = recipes[0].imagemIntrucao[count]
+        ContDeInstrucoes.text = "Passo \(count+1) de \(recipes[0].numeroIntrucoes)"
+        if recipes[0].dicas[count] == ""{
+            LampImage.isHidden = true
+        }
        
        
     }
