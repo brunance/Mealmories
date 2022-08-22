e952d1171ed7f9a1ba3e46e2b213919ae7745582
gabrieldigon <gabrieldiasgonca@gmail.com>
2022-08-22 15:36:48 -0400

    Applying the redesign and creating the Function Next Step

--- Challenge04LGBB/Controller/RecipeViewController.swift
+++ Challenge04LGBB/Controller/RecipeViewController.swift
@@ -16,18 +16,16 @@ class RecipeViewController: UIViewController{
     @IBOutlet weak var viewTurno: UIView!
     @IBOutlet weak var labelContagemTurno: UILabel!
 
-    @IBOutlet weak var ImagemInstrucao: UIImageView!
-    @IBOutlet weak var LInhaDivisoria: UIView!
+    @IBOutlet weak var bottomColor: UIView!
     @IBOutlet weak var statusbar: UIView!
-    @IBOutlet weak var ContDeInstrucoes: UILabel!
+    
     
     @IBOutlet weak var LampImage: UIImageView!
     @IBOutlet weak var LabelDica: UILabel!
     
     override func viewDidLoad() {
        
-        viewTurno.layer.cornerRadius = 15
-        viewTurno.layer.borderWidth = 5
+     
         
         recipes = [
             Recipe(tituloReceita: "Pão de Queijo", numeroIntrucoes: 9, pessoaTurno: ["Adulto", "Criança", "Mix", "Adulto", "Mix", "Criança", "Mix", "Adulto", "Adulto"], descricaoReceita: ["Em uma vasilha, junte os ingredientes secos: polvilho doce, e parmesão", "misture bem com uma colher ou com a mão!", "Adicione o creme de leite, aos poucos, misturando com as mãos até formar uma massa homogênea e firme.","Retire porções pequenas da massa", "modele do formato que quiser, bolinhas, dadinhos, seja criativo!", "Unte uma fôrma com manteiga e trigo, papel manteiga ou spray de untar ", "Coloque uma ao lado da outra na fôrma grande untada", "Leve ao forno alto, preaquecido a 180°C , por 15 minutos ou até dourar.", "Retire e sirva em seguida."], numeroEtapas: 3,
@@ -47,19 +45,16 @@ class RecipeViewController: UIViewController{
         labelTurno.text = "Turno \(recipes[0].pessoaTurno[count])"
         labelIntrucao.text = "\(recipes[0].descricaoReceita[count])"
         LabelDica.text = "\(recipes[0].dicas[count])"
-        navigationController?.navigationBar.backgroundColor = recipes[0].CorDaTela[count]
         statusbar.backgroundColor = recipes[0].CorDaTela[count]
-        viewTurno.layer.borderColor = recipes[0].CorDaTela[count].cgColor
-        LInhaDivisoria.backgroundColor = recipes[0].CorDaTela[count]
         imagemIntrucao.image = recipes[0].imagemIntrucao[count]
-        ContDeInstrucoes.text = "Passo \(count+1) de \(recipes[0].numeroIntrucoes)"
+        bottomColor.backgroundColor = recipes[0].CorDaTela[count]
         
         if recipes[0].dicas[count] == ""{
             LampImage.isHidden = true
         }
         
         for i in 1...5{
-            let view = UIView(frame: CGRect(x: cont, y: Int(progressBar.frame.height)/2, width: 50, height: 10))
+            let view = UIView(frame: CGRect(x: cont, y: Int(progressBar.frame.height)/2, width: 10, height: 10))
             if i < count + 2{
                 view.backgroundColor = recipes[0].CorDaTela[count]
             } else {
@@ -78,18 +73,7 @@ class RecipeViewController: UIViewController{
         }
         else{
         count += 1
-            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let newViewController = storyBoard.instantiateViewController(withIdentifier: "Main") as! RecipeViewController
-            
-
-            let transition = CATransition()
-                transition.duration = 0.4
-                transition.type = CATransitionType.reveal
-                transition.subtype = CATransitionSubtype.fromRight
-                guard let window = view.window else { return }
-                window.layer.add(transition, forKey: kCATransition)
-            newViewController.modalPresentationStyle = .fullScreen
-            self.present(newViewController, animated: false, completion: nil)
+            NextStep()
         }
       
     }
@@ -100,6 +84,11 @@ class RecipeViewController: UIViewController{
         }
         else{
         count -= 1
+        NextStep()
+        }
+    }
+    
+    func NextStep(){
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let newViewController = storyBoard.instantiateViewController(withIdentifier: "Main") as! RecipeViewController
         
@@ -107,11 +96,10 @@ class RecipeViewController: UIViewController{
         let transition = CATransition()
             transition.duration = 0.4
             transition.type = CATransitionType.reveal
-            transition.subtype = CATransitionSubtype.fromLeft
+            transition.subtype = CATransitionSubtype.fromRight
             guard let window = view.window else { return }
             window.layer.add(transition, forKey: kCATransition)
         newViewController.modalPresentationStyle = .fullScreen
         self.present(newViewController, animated: false, completion: nil)
-        }
     }
 }
