ad541fd58e8347eb0e933f2a8ae25ba2779ce773
Bruno França do Prado <54959394+brunance@users.noreply.github.com>
2022-08-25 09:29:48 -0400

    Revert "Merge pull request #19 from brunance/FeatureConfig"
    This reverts commit 97bec6b5763073efc6a4f76881b151dabef2e18d.

--- Challenge04LGBB/Controller/RecipeViewController.swift
+++ Challenge04LGBB/Controller/RecipeViewController.swift
@@ -1,18 +1,14 @@
 import Foundation
 import UIKit
-import ARKit
 
 var count = 0
 var progressBarCount = 0
-var eye = false
 
-class RecipeViewController: UIViewController, ARSCNViewDelegate{
+class RecipeViewController: UIViewController{
     
     @IBOutlet weak var ProgressBarAux: UIStackView!
     var recipes : [Recipe] = []
     var xspace = 5
-    var analysis = ""
-    var texto = ""
     
     @IBOutlet weak var botaovoltar: UIButton!
     
@@ -35,68 +31,10 @@ class RecipeViewController: UIViewController, ARSCNViewDelegate{
     @IBOutlet weak var LampImage: UIImageView!
     @IBOutlet weak var LabelDica: UILabel!
     
-    @IBOutlet weak var sceneView: ARSCNView!
-    
-    override func viewWillAppear(_ animated: Bool) {
-           super.viewWillAppear(animated)
-           
-           let configuration = ARFaceTrackingConfiguration()
-           sceneView.session.run(configuration)
-           sceneView.preferredFramesPerSecond = 10
-           sceneView.isHidden = true
-       }
-       // MARK: - ARSCNViewDelegate
-       
-       func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-           let faceMesh = ARSCNFaceGeometry(device: sceneView.device!)
-           let node = SCNNode(geometry: faceMesh)
-           node.geometry?.firstMaterial?.fillMode = .lines
-           return node
-       }
-       
-       func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-           if let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry {
-               faceGeometry.update(from: faceAnchor.geometry)
-               expression(anchor: faceAnchor)
-               
-               DispatchQueue.main.async {
-                   self.texto = self.analysis
-                   
-                   if (self.texto == "You are blinking right." && count < self.recipes[0].numeroIntrucoes - 1){
-                       if count < 9 {
-                           count += 1
-                       }
-                       self.NextStep()
-                       print(count)
-                   }
-                   if(self.texto == "You are blinking left." && count != 0){
-                       count -= 1
-                       self.LastStep()
-                       print(count)
-                   }
-                   else{
-                       print("nao esta piscando")
-                   }
-                   
-               }
-           }
-       }
-       
-       func expression(anchor: ARFaceAnchor) {
-           let eyeblinkright = anchor.blendShapes[.eyeBlinkRight]
-           let eyeblinkleft = anchor.blendShapes[.eyeBlinkLeft]
-           self.analysis = ""
-           
-           if eyeblinkright?.decimalValue ?? 0.0 > 0.7 {
-               self.analysis += "You are blinking left."
-           }
-           if eyeblinkleft?.decimalValue ?? 0.0 > 0.7 {
-               self.analysis += "You are blinking right."
-           }
-       }
-    
     override func viewDidLoad() {
        
+     
+        
         recipes = [
             Recipe(tituloReceita: "Pão de Queijo", numeroIntrucoes: 9, pessoaTurno: ["Adulto", "Criança", "Mix", "Adulto", "Mix", "Criança", "Mix", "Adulto", "Adulto"], descricaoReceita: ["Em uma vasilha, junte os ingredientes secos: polvilho doce, e parmesão", "Misture bem com uma colher ou com a mão!", "Adicione o creme de leite, aos poucos, misturando com as mãos até formar uma massa homogênea e firme.","Retire porções pequenas da massa", "Modele do formato que quiser, bolinhas, dadinhos, seja criativo!", "Unte uma fôrma com manteiga e trigo, papel manteiga ou spray de untar ", "Coloque uma ao lado da outra na fôrma grande untada", "Leve ao forno alto, preaquecido a 180°C , por 15 minutos ou até dourar.", "Retire e sirva em seguida."], numeroEtapas: 3,
                    imagemIntrucao:
@@ -110,7 +48,6 @@ class RecipeViewController: UIViewController, ARSCNViewDelegate{
                      ,UIImage(named: "forno.1")!,
                      UIImage(named: "paodequeijo.1")!]
                    ,CorDaTela: [UIColor(named: "Adulto_Blue")!,UIColor(named: "Child_Orange")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Adulto_Blue")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Child_Orange")!,UIColor(named: "Mix_Magenta")!,UIColor(named: "Adulto_Blue")!,UIColor(named: "Adulto_Blue")!],dicas: ["Qualquer dos dois tipos de polvilhos são bem-vindos, variando de acordo com o gosto de quem manuseia a receita","","É necessário que o creme de leite seja adicionado aos poucos, para não perder o ponto da massa.","","É necessário que o creme de leite seja adicionado aos poucos, para não perder o ponto da massa.","Pode haver ajuda do adulto na orientação, mas é interessante que a criança faça sozinha.","Deixe sempre um pequeno espaço entre as massas , pode facilitar na hora de retirar os pães.","",""],CorDoFundoDatela: [UIColor(named: "LabelBlue")!,UIColor(named: "LabelOrange")!,UIColor(named: "LabelMagenta")!,UIColor(named: "LabelBlue")!,UIColor(named: "LabelMagenta")!,UIColor(named: "LabelOrange")!,UIColor(named: "LabelMagenta")!,UIColor(named: "LabelBlue")!,UIColor(named: "LabelBlue")!],Etapas: [1,1,1,2,2,3,3,3,3],InstruçõesPorEtapa: [3,3,3,2,2,4,4,4,4])]
-        
         botaoir.backgroundColor = recipes[0].CorDaTela[count]
         botaovoltar.backgroundColor = recipes[0].CorDaTela[count]
         viewTurno.backgroundColor = recipes[0].CorDoFundoDatela[count]
@@ -133,7 +70,7 @@ class RecipeViewController: UIViewController, ARSCNViewDelegate{
         
         for i in 1...recipes[0].InstruçõesPorEtapa[count]{
             let view = UIView(frame: CGRect(x: xspace, y: Int(progressBar.frame.height)/2, width: 20, height: 20))
-            
+            if recipes[0].Etapas[count+1]>recipes[0].Etapas[count]{progressBarCount = 0}
             if i < progressBarCount + 2{
                 view.backgroundColor = recipes[0].CorDaTela[count]
             } else {
@@ -144,11 +81,11 @@ class RecipeViewController: UIViewController, ARSCNViewDelegate{
             xspace += 22
         }
         
-        sceneView.delegate = self
-        guard ARFaceTrackingConfiguration.isSupported else {
-            fatalError("Face tracking is not supported on this device")
-        }
+       
     }
+   
+  
+    
    
    
     @IBAction func NextStep(_ sender: Any) {
