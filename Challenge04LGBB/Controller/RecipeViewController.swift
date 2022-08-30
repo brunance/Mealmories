2c6a77ac04000f45c1f5a71c084f98dfe66a0bb8
gabrieldigon <gabrieldiasgonca@gmail.com>
2022-08-30 10:12:05 -0400

    progressbAr

--- Challenge04LGBB/Controller/RecipeViewController.swift
+++ Challenge04LGBB/Controller/RecipeViewController.swift
@@ -107,22 +107,8 @@ class RecipeViewController: UIViewController, ARSCNViewDelegate{
 //            self.analysis += "You are blinking right."
 //        }
 //    }
-    
-    override func viewDidLoad() {
-        
-        recipes = getRecipes()
-        print("\n\n\n\n\n o count é \(count)")
-        updateData()
-//        
-//        for i in 1...recipes[0].InstruçõesPorEtapa[count]{
-//            let view = UIView(frame: CGRect(x: xspace, y: Int(progressBar.frame.height)/2, width: 20, height: 20))
-//            view.backgroundColor = recipes[0].CorDaTela[count]
-//            view.layer.cornerRadius = 10
-//            view.layer.zPosition = 1
-//            progressBar.addSubview(view)
-//            xspace += 22
-//        }
-        for i in 1...recipes[0].auxiliarInstrucoesPorEtapas[count]{
+    override func viewWillAppear(_ animated: Bool) {
+        for i in 1...9{
             let view = UIView(frame: CGRect(x: xspaceaux, y: Int(ProgressBarAux.frame.height)/2, width: 20, height: 20))
             view.backgroundColor = UIColor.systemGray
             view.layer.cornerRadius = 10
@@ -130,7 +116,22 @@ class RecipeViewController: UIViewController, ARSCNViewDelegate{
             progressBar.addSubview(view)
             xspaceaux += 22
         }
-//
+    }
+    override func viewDidLoad() {
+        
+        recipes = getRecipes()
+        print("\n\n\n\n\n o count é \(count)")
+        updateData()
+        xspace = 5
+        for i in 1...recipes[0].auxiliarInstrucoesPorEtapas[count]{
+        let view = UIView(frame: CGRect(x: xspace, y: Int(progressBar.frame.height)/2, width: 20, height: 20))
+            view.backgroundColor = UIColor(named: "Mix_DarkMagenta")
+        view.layer.cornerRadius = 10
+        view.layer.zPosition = 1
+        progressBar.addSubview(view)
+        xspace += 22
+    }
+  
 //
 //
 //        sceneView.delegate = self
